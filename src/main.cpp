054223b7e097dffa77de26470ca0937081d82ca1
hfp-br <hfppdn@gmail.com>
2026-03-13 20:19:08 -0300

    testes iniciais com fisica

--- src/main.cpp
+++ src/main.cpp
@@ -2,105 +2,85 @@
 #include <box2d/box2d.h>
 #include <cmath>
 
-// converte metros da física para pixels na tela
-static constexpr float PIXELS_PER_METER = 100.0f;
-
-static Vector2 ToScreen(b2Vec2 position)
-{
-    return Vector2{
-        position.x * PIXELS_PER_METER,
-        position.y * PIXELS_PER_METER
-    };
-}
+#define scale 10
 
 int main()
 {
+    //seta o tamanho da tela
     const int screenWidth = 1280;
     const int screenHeight = 720;
 
-    InitWindow(screenWidth, screenHeight, "raylib + box2d teste");
+    
+    //nome da janela e FPS
+    InitWindow(screenWidth, screenHeight, "long prep");
     SetTargetFPS(60);
+    
+    Texture2D texture = LoadTexture("images/open backpack.png");
 
-    // cria mundo da física
     b2WorldDef worldDef = b2DefaultWorldDef();
     worldDef.gravity = {0.0f, 9.8f};
     b2WorldId world = b2CreateWorld(&worldDef);
 
-    // cria o chão
-    b2BodyDef groundBodyDef = b2DefaultBodyDef();
-    groundBodyDef.position = {6.4f, 6.5f};
-    b2BodyId groundBody = b2CreateBody(world, &groundBodyDef);
+    // cria o formato basico da mochila
 
-    b2Polygon groundBox = b2MakeBox(5.0f, 0.25f);
+    //parte inferior
+    b2BodyDef groundBodyDef = b2DefaultBodyDef(); //definebody
+    groundBodyDef.position = {(screenWidth/2-175)/scale, (screenHeight/2+275)/scale}; //define posicao inicial
+    b2BodyId groundBody = b2CreateBody(world, &groundBodyDef); //cria o "perfil" no mundo
+    b2Polygon groundBox = b2MakeBox(20.0f, 0.5f); //cria uma caixa com o tamanho definido
+    b2ShapeDef groundShapeDef = b2DefaultShapeDef(); //coloca o formato como quadrado padrao do box2d
+    b2CreatePolygonShape(groundBody, &groundShapeDef, &groundBox); //aplica o formato e a caixa em um poligono
 
-    b2ShapeDef groundShapeDef = b2DefaultShapeDef();
-    b2CreatePolygonShape(groundBody, &groundShapeDef, &groundBox);
+    //parte esquerda
+    b2BodyDef leftBodyDef = b2DefaultBodyDef(); //definebody
+    leftBodyDef.position = {(screenWidth/2-135)/scale, (screenHeight/2-125)/scale}; //define posicao inicial
+    b2BodyId leftBody = b2CreateBody(world, &leftBodyDef); //cria o "perfil" no mundo
+    b2Polygon leftBox = b2MakeBox(0.5f, 20); //cria uma caixa com o tamanho definido
+    b2ShapeDef leftShapeDef = b2DefaultShapeDef(); //coloca o formato como quadrado padrao do box2d
+    b2CreatePolygonShape(leftBody, &leftShapeDef, &leftBox); //aplica o formato e a caixa em um poligono
 
-    // cria a caixa dinâmica
-    b2BodyDef boxBodyDef = b2DefaultBodyDef();
-    boxBodyDef.type = b2_dynamicBody;
-    boxBodyDef.position = {6.4f, 2.0f};
-    b2BodyId boxBody = b2CreateBody(world, &boxBodyDef);
-
-    b2Polygon boxShape = b2MakeBox(0.3f, 0.3f);
-
-    b2ShapeDef boxShapeDef = b2DefaultShapeDef();
-    boxShapeDef.material.friction = 0.4f;
-    boxShapeDef.material.restitution = 0.1f;
-    boxShapeDef.density = 1.0f;
-    b2CreatePolygonShape(boxBody, &boxShapeDef, &boxShape);
-
+    //parte direita
+    b2BodyDef rightBodyDef = b2DefaultBodyDef(); //definebody
+    rightBodyDef.position = {(screenWidth/2+185)/scale, (screenHeight/2-125)/scale}; //define posicao inicial
+    b2BodyId rightBody = b2CreateBody(world, &rightBodyDef); //cria o "perfil" no mundo
+    b2Polygon rightBox = b2MakeBox(0.5f, 20); //cria uma caixa com o tamanho definido
+    b2ShapeDef rightShapeDef = b2DefaultShapeDef(); //coloca o formato como quadrado padrao do box2d
+    b2CreatePolygonShape(rightBody, &rightShapeDef, &rightBox); //aplica o formato e a caixa em um poligono
+    
+    //caixa de teste para colisao
+    b2BodyDef bodyDef = b2DefaultBodyDef();
+    bodyDef.type = b2_dynamicBody;
+    bodyDef.position = {(screenWidth/2)/scale, (screenHeight/2-100)/scale};
+    b2BodyId bodyId = b2CreateBody(world, &bodyDef);
+    b2Polygon dynamicBox = b2MakeBox(1.0f, 1.0f);
+    b2ShapeDef shapeDef = b2DefaultShapeDef();
+    shapeDef.density = 1.0f;
+    b2CreatePolygonShape(bodyId, &shapeDef, &dynamicBox);
+    
+    
     while (!WindowShouldClose())
     {
-        float dt = GetFrameTime();
+        b2Vec2 boxPos = b2Body_GetPosition(bodyId);
+    
+        int drawX = (boxPos.x - 1.0f) * scale;
+        int drawY = (boxPos.y - 1.0f) * scale;
+        int drawWidth = 2.0f * scale;
+        int drawHeight = 2.0f * scale;
 
-        // atualiza física
+        float dt = GetFrameTime();
         b2World_Step(world, dt, 4);
 
-        // pega posição e rotação da caixa
-        b2Vec2 boxPos = b2Body_GetPosition(boxBody);
-        b2Rot boxRot = b2Body_GetRotation(boxBody);
-
-        Vector2 screenPos = ToScreen(boxPos);
-        float angleDeg = atan2f(boxRot.s, boxRot.c) * RAD2DEG;
-
         BeginDrawing();
-        ClearBackground(BLACK);
+        ClearBackground(WHITE);
 
-        DrawText("se a caixa cair e parar no chao, deu certo", 30, 30, 20, RAYWHITE);
+        DrawTextureEx(texture, (Vector2){(screenWidth/2)-250, screenHeight/2-200}, 0.0f, 0.55,WHITE);
+        DrawRectangle(screenWidth/2-175,screenHeight/2+265,400,10,RED);
+        DrawRectangle(screenWidth/2-135,screenHeight/2-125,10,400,RED);
+        DrawRectangle(screenWidth/2+185,screenHeight/2-125,10,400,RED);
 
-        // desenha chão
-        DrawRectanglePro(
-            Rectangle{
-                6.4f * PIXELS_PER_METER,
-                6.5f * PIXELS_PER_METER,
-                10.0f * PIXELS_PER_METER,
-                0.5f * PIXELS_PER_METER
-            },
-            Vector2{
-                5.0f * PIXELS_PER_METER,
-                0.25f * PIXELS_PER_METER
-            },
-            0.0f,
-            DARKGRAY
-        );
 
-        // desenha caixa
-        DrawRectanglePro(
-            Rectangle{
-                screenPos.x,
-                screenPos.y,
-                0.6f * PIXELS_PER_METER,
-                0.6f * PIXELS_PER_METER
-            },
-            Vector2{
-                0.3f * PIXELS_PER_METER,
-                0.3f * PIXELS_PER_METER
-            },
-            angleDeg,
-            RED
-        );
 
+        DrawRectangle(drawX, drawY, drawWidth, drawHeight, GREEN);
         EndDrawing();
     }
 
