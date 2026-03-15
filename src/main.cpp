07a63736bdd25f5bbadd6fcd21967fc8439e2214
hfp-br <hfppdn@gmail.com>
2026-03-14 22:47:25 -0300

    otimizacao do codigo

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,98 @@
 #include "box2d/id.h"
 #include "box2d/math_functions.h"
+#include "box2d/types.h"
 #include <raylib.h>
 #include <box2d/box2d.h>
 #include <cmath>
 #include <iostream>
+#include <vector>
 
 #define scale 10
-bool segurado=false;
 
-void click(b2BodyId id){
+bool MousePressed = false;
 
+//struct para guardar qual objeto tem qual tamanho (usado para rendering)
+typedef struct {
+    float width;
+    float height;
+    float radius;
+    char itemType[30];
+    b2BodyId id;
+} physicalObject;
 
-    b2Vec2 boxPos = b2Body_GetPosition(id);
-    float drawX = (boxPos.x - 1.0f) * scale;
-    float drawY = (boxPos.y - 1.0f) * scale;
-    float drawWidth = 2.0f * scale;
-    float drawHeight = 2.0f * scale;
+//gerador rapido de paredes
+physicalObject WallGenerator(b2WorldId world, float xP, float yP,float width, float height, enum b2BodyType type){ 
+    b2BodyDef bodyDef = b2DefaultBodyDef(); //definebody 
+    bodyDef.position = {(xP)/scale, (yP)/scale}; //define posicao inicial 
+    bodyDef.type = type;
+    b2BodyId bodyId = b2CreateBody(world, &bodyDef); //cria o "perfil" no mundo 
+    b2Polygon bodyBox = b2MakeBox((width/2)/scale, (height/2)/scale); //cria uma caixa com o tamanho definido 
+    b2ShapeDef bodyshapeDef = b2DefaultShapeDef(); //coloca o formato como quadrado padrao do box2d 
+    b2CreatePolygonShape(bodyId, &bodyshapeDef, &bodyBox); //aplica o formato e a caixa em um poligono 
+    physicalObject Parede;
+    Parede.width = width;
+    Parede.height = height;
+    Parede.id = bodyId;
 
-    b2Vec2 mouseWorld = {
-    (float)GetMouseX() / scale,
-    (float)GetMouseY() / scale
-    };
-
-    b2Vec2 direcao = {
-    mouseWorld.x - boxPos.x,
-    mouseWorld.y - boxPos.y
-    };
-
-    b2Vec2 vel = {
-    direcao.x * 15,
-    direcao.y * 15
-    };
-    
-    if(IsMouseButtonDown(0)){
-        Rectangle mouseHitbox = (Rectangle){(float)GetMouseX(),(float)GetMouseY(),5,5};
-
-
-        if(CheckCollisionRecs(mouseHitbox, (Rectangle){drawX, drawY, drawWidth, drawHeight})){
-            segurado=true;
-        }
-    }
-    if(IsMouseButtonReleased(0)){
-        segurado=false;
-        std::cout << "ta solto" << std::endl;
-    }
-    
-    if(segurado==true){
-        
-        std::cout << "AAAAAAAAAAAAI" << std::endl;
-        
-        b2Body_SetLinearVelocity(id, vel);
-    }
+    return Parede;
 }
 
+physicalObject ItemGenerator(b2WorldId world, bool isCircle ,float xP, float yP,float width, float height, float radius, enum b2BodyType type){ 
+    b2BodyDef bodyDef = b2DefaultBodyDef(); //definebody 
+    bodyDef.position = {(xP)/scale, (yP)/scale}; //define posicao inicial 
+    bodyDef.type = type;
+    b2BodyId bodyId = b2CreateBody(world, &bodyDef); //cria o "perfil" no mundo 
+    if(!isCircle){
+        b2Polygon bodyBox = b2MakeBox((width/2)/scale, (height/2)/scale); //cria uma caixa com o tamanho definido 
+        b2ShapeDef bodyshapeDef = b2DefaultShapeDef(); //coloca o formato como quadrado padrao do box2d 
+        bodyshapeDef.density = 1.0f;
+        b2CreatePolygonShape(bodyId, &bodyshapeDef, &bodyBox); //aplica o formato e a caixa em um poligono 
+    } else {
+        b2Circle circle;
+        circle.center = {0.0f, 0.0f};
+        circle.radius = radius/scale;
+        b2ShapeDef ballshapeDef = b2DefaultShapeDef();
+        ballshapeDef.density = 1.0f;
+        b2CreateCircleShape(bodyId, &ballshapeDef, &circle);
+    }
+    physicalObject Item;
+    Item.width = width;
+    Item.height = height;
+    Item.radius = radius;
+    Item.id = bodyId;
+
+    return Item;
+}
+
+void Grab(physicalObject body,float reverseWeight){
+    Rectangle mouseHitbox = (Rectangle){(float)GetMouseX()/scale,(float)GetMouseY()/scale,5,5};
+
+    b2Vec2 pos = b2Body_GetPosition(body.id);
+
+    float x = pos.x * scale;
+    float y = pos.y * scale;
+
+    b2Vec2 direction;
+    direction.x = mouseHitbox.x - pos.x;
+    direction.y = mouseHitbox.y - pos.y;
+
+    b2Vec2 speed;
+    speed.x = direction.x * reverseWeight;
+    speed.y = direction.y * reverseWeight;
+
+    if(MousePressed==true){
+        b2Body_SetLinearVelocity(body.id, speed);
+    }
+
+    if(CheckCollisionCircleRec((Vector2){pos.x,pos.y}, body.radius/scale, mouseHitbox)){
+        if(IsMouseButtonDown(0)){
+            MousePressed=true;
+        }
+        else{
+            MousePressed=false;
+        }
+    }
+}
 
 int main()
 {
@@ -64,108 +104,47 @@ int main()
     //nome da janela e FPS
     InitWindow(screenWidth, screenHeight, "long prep");
     SetTargetFPS(60);
-    
-    Texture2D texture = LoadTexture("images/open backpack.png");
 
     b2WorldDef worldDef = b2DefaultWorldDef();
     worldDef.gravity = {0.0f, 9.8f};
     b2WorldId world = b2CreateWorld(&worldDef);
 
-    // cria o formato basico da mochila
-
-    //parte inferior
-    b2BodyDef groundBodyDef = b2DefaultBodyDef(); //definebody
-    groundBodyDef.position = {(screenWidth/2)/scale, (screenHeight/2+275)/scale}; //define posicao inicial
-    b2BodyId groundBody = b2CreateBody(world, &groundBodyDef); //cria o "perfil" no mundo
-    b2Polygon groundBox = b2MakeBox(20.0f, 0.5f); //cria uma caixa com o tamanho definido
-    b2ShapeDef groundShapeDef = b2DefaultShapeDef(); //coloca o formato como quadrado padrao do box2d
-    b2CreatePolygonShape(groundBody, &groundShapeDef, &groundBox); //aplica o formato e a caixa em um poligono
-
-    //parte esquerda
-    b2BodyDef leftBodyDef = b2DefaultBodyDef(); //definebody
-    leftBodyDef.position = {(screenWidth/2-135)/scale, (screenHeight/2+100)/scale}; //define posicao inicial
-    b2BodyId leftBody = b2CreateBody(world, &leftBodyDef); //cria o "perfil" no mundo
-    b2Polygon leftBox = b2MakeBox(0.5f, 20); //cria uma caixa com o tamanho definido
-    b2ShapeDef leftShapeDef = b2DefaultShapeDef(); //coloca o formato como quadrado padrao do box2d
-    b2CreatePolygonShape(leftBody, &leftShapeDef, &leftBox); //aplica o formato e a caixa em um poligono
-
-    //parte direita
-    b2BodyDef rightBodyDef = b2DefaultBodyDef(); //definebody
-    rightBodyDef.position = {(screenWidth/2+185)/scale, (screenHeight/2+100)/scale}; //define posicao inicial
-    b2BodyId rightBody = b2CreateBody(world, &rightBodyDef); //cria o "perfil" no mundo
-    b2Polygon rightBox = b2MakeBox(0.5f, 20); //cria uma caixa com o tamanho definido
-    b2ShapeDef rightShapeDef = b2DefaultShapeDef(); //coloca o formato como quadrado padrao do box2d
-    b2CreatePolygonShape(rightBody, &rightShapeDef, &rightBox); //aplica o formato e a caixa em um poligono
+    std::vector<physicalObject> BackpackWalls;
+    BackpackWalls.push_back(WallGenerator(world, screenWidth/2, screenHeight/2+275, 400, 10,b2_staticBody));
+    BackpackWalls.push_back(WallGenerator(world, screenWidth/2-135, screenHeight/2+100, 10, 400,b2_staticBody));
+    BackpackWalls.push_back(WallGenerator(world, screenWidth/2+180, screenHeight/2+100, 10, 400,b2_staticBody));
     
-    //caixa de teste para colisao
-    b2BodyDef bodyDef = b2DefaultBodyDef();
-    bodyDef.type = b2_dynamicBody;
-    bodyDef.position = {(screenWidth/2)/scale+6, (screenHeight/2-100)/scale};
-    b2BodyId bodyId = b2CreateBody(world, &bodyDef);
-    b2Polygon dynamicBox = b2MakeBox(1.0f, 1.0f);
-    b2ShapeDef shapeDef = b2DefaultShapeDef();
-    shapeDef.density = 1.0f;
-    b2CreatePolygonShape(bodyId, &shapeDef, &dynamicBox);
-
-    //caixa de teste para colisao
-    b2BodyDef ballbodyDef = b2DefaultBodyDef();
-    b2Circle circle;
-    circle.center = {0.0f, 0.0f};
-    circle.radius = 1.0f;
-    ballbodyDef.type = b2_dynamicBody;
-    ballbodyDef.position = {((screenWidth/2))/scale+5, (screenHeight/2-50)/scale};
-    b2BodyId ballbodyId = b2CreateBody(world, &ballbodyDef);
-    b2ShapeDef ballshapeDef = b2DefaultShapeDef();
-    ballshapeDef.density = 1.0f;
-    b2CreateCircleShape(ballbodyId, &ballshapeDef, &circle);
-    
+    physicalObject ball = ItemGenerator(world, true, screenWidth/2, screenHeight/2-100, 0, 0, 10, b2_dynamicBody);
     
     while (!WindowShouldClose())
     {
-        b2Vec2 boxPos = b2Body_GetPosition(bodyId);
-        b2Vec2 ballPos = b2Body_GetPosition(ballbodyId);
-        int bdrawX = (ballPos.x) * scale;
-        int bdrawY = (ballPos.y) * scale;
-        b2Vec2 groundPos = b2Body_GetPosition(groundBody);
-        int groundDrawX = (groundPos.x - 20.0f) * scale;
-        int groundDrawY = (groundPos.y - 0.5f) * scale;
-        int groundDrawWidth = 40.0f * scale;
-        int groundDrawHeight = 1.0f * scale;
-
-        b2Vec2 leftPos = b2Body_GetPosition(leftBody);
-        int leftDrawX = (leftPos.x - 0.5f) * scale;
-        int leftDrawY = (leftPos.y - 20.0f) * scale;
-        int leftDrawWidth = 1.0f * scale;
-        int leftDrawHeight = 40.0f * scale;
-
-        b2Vec2 rightPos = b2Body_GetPosition(rightBody);
-        int rightDrawX = (rightPos.x - 0.5f) * scale;
-        int rightDrawY = (rightPos.y - 20.0f) * scale;
-        int rightDrawWidth = 1.0f * scale;
-        int rightDrawHeight = 40.0f * scale;
-        int drawX = (boxPos.x - 1.0f) * scale;
-        int drawY = (boxPos.y - 1.0f) * scale;
-        int drawWidth = 2.0f * scale;
-        int drawHeight = 2.0f * scale;
-
         float dt = GetFrameTime();
-        click(bodyId);
-
+        
         b2World_Step(world, dt, 4);
-
+        
+        Grab(ball,10);
 
         BeginDrawing();
         ClearBackground(WHITE);
+        
+        for(const physicalObject& body: BackpackWalls){
+            b2Vec2 pos = b2Body_GetPosition(body.id);
 
-        DrawTextureEx(texture, (Vector2){(screenWidth/2)-250, screenHeight/2-200}, 0.0f, 0.55,WHITE);
-        DrawRectangle(groundDrawX, groundDrawY, groundDrawWidth, groundDrawHeight, RED);
-        DrawRectangle(leftDrawX, leftDrawY, leftDrawWidth, leftDrawHeight, RED);
-        DrawRectangle(rightDrawX, rightDrawY, rightDrawWidth, rightDrawHeight, RED);
+            float x = pos.x * scale;
+            float y = pos.y * scale;
 
+            DrawRectangle(x - body.width/2, y - body.height/2, body.width, body.height, RED);
+        }
 
+        for(int i=0; i<1; i++){
+            b2Vec2 pos = b2Body_GetPosition(ball.id);
+    
+            float x = pos.x * scale;
+            float y = pos.y * scale;
+    
+            DrawCircle(x, y, ball.radius, GREEN);
+        }
 
-        DrawRectangle(drawX, drawY, drawWidth, drawHeight, GREEN);
-        DrawCircle(bdrawX, bdrawY, 1*scale, GREEN);
         EndDrawing();
     }
 
